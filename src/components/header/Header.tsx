acf081e92e4d64dbe47231f08527b184880a1dbb
Julio <jcfmneto@gmail.com>
2025-07-25 16:35:38 -0300

    Ajustando a cor do Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (<div className="pt-1 bg-gray-700 h-screen flex justify-center">
-    <header className="w-[80vw] h-[10vh] bg-gray-900 rounded-3xl mx-auto"></header>
+    <header className="w-[80vw] h-[12vh] bg-gray-900 rounded-3xl mx-auto shadow-lg shadow-blue-950/90"></header>
   </div>
   
   );
