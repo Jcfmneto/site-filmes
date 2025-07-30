256a94834409d963b8539cea08332325ba9fbdb2
Julio <jcfmneto@gmail.com>
2025-07-29 22:05:32 -0300

    Implementando o autocomplete

--- src/components/header/SearchInput.tsx
+++ src/components/header/SearchInput.tsx
@@ -1,28 +1,89 @@
-import SearchIcon from "@heroicons/react/24/solid/MagnifyingGlassIcon"
-import { useEffect, useRef } from "react"
+import SearchIcon from "@heroicons/react/24/solid/MagnifyingGlassIcon";
+import { useState, useRef } from "react";
+import { tmdbApi } from "../../services/api";
+import type { Movie } from "../../types/Movie";
 
 const SearchInput = () => {
-  const inputRef = useRef(null);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const timeRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const [results, setResults] = useState<Movie[]>([]);
+  const [openSearch, setOpenSearch] = useState<boolean>(false);
 
-  useEffect(() => {})
+  const handleInput = () => {
+    const query = inputRef.current?.value;
 
+    if (timeRef.current) {
+      clearTimeout(timeRef.current);
+    }
+
+    timeRef.current = setTimeout(async () => {
+      if (query && query.trim() !== "") {
+        try {
+          const response = await tmdbApi.get("/search/movie", {
+            params: { query },
+          });
+          setResults(response.data.results);
+          setOpenSearch(true);
+        } catch (error) {
+          console.error("Erro ao buscar filmes:", error);
+          setOpenSearch(false);
+        }
+      } else {
+        setResults([]);
+        setOpenSearch(false);
+      }
+    }, 1000);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
 
   return (
-    <form className="flex items-center justify-between border border-white rounded overflow-hidden">
-      <input
-        type="text"
-        ref={inputRef}
-        placeholder="Search movies"
-        className="flex-1  min-w-0  max-w-4xl bg-transparent text-white border-none focus:outline-none
-          text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl
-          px-2 sm:px-3 md:px-4 lg:px-5 xl:px-6
-          py-1 sm:py-1.5 md:py-2 lg:py-2.5 xl:py-3"
-      />
-      <button type="submit" className=" pr-2  flex-shrink-0 flex justify-center items-center">
-        <SearchIcon className="h-3 w-3 text-white sm:h-6 sm:w-6 md:h-7 md:w-7 lg:h-8 lg:w-8 hover:cursor-pointer" />
-      </button>
-    </form>
-  )
-}
+    <>
+      <div className="relative max-w-4xl mx-auto">
+        <form
+          className="flex items-center justify-between border border-white rounded overflow-hidden"
+          onSubmit={handleSubmit}
+        >
+          <input
+            type="text"
+            ref={inputRef}
+            placeholder="Search movies"
+            onChange={handleInput}
+            className="flex-1  min-w-0  max-w-4xl bg-transparent text-white border-none focus:outline-none
+            text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl
+            px-2 sm:px-3 md:px-4 lg:px-5 xl:px-6
+            py-1 sm:py-1.5 md:py-2 lg:py-2.5 xl:py-3"
+          />
+          <button
+            type="submit"
+            className=" pr-2  flex-shrink-0 flex justify-center items-center"
+          >
+            <SearchIcon className="h-3 w-3 text-white sm:h-6 sm:w-6 md:h-7 md:w-7 lg:h-8 lg:w-8 hover:cursor-pointer" />
+          </button>
+        </form>
 
-export default SearchInput
+        <ul
+          className={`absolute left-0 right-0 z-10 mt-1 max-h-60 overflow-auto rounded bg-black bg-opacity-80 transition-opacity duration-300 ${
+            openSearch
+              ? "opacity-100 translate-y-0"
+              : "opacity-0 translate-y-[-10px] pointer-events-none"
+          }`}
+        >
+          {results.slice(0, 5).map((movie) => (
+            <li
+              key={movie.id}
+              className="px-4 py-2 border-b border-gray-600 last:border-b-0 text-white"
+            >
+              <strong>Título:</strong> {movie.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default SearchInput;
+
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
-const token = import.meta.env.VITE_API_KEY;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 export const tmdbApi = axios.create({
-  baseURL: "https://api.themoviedb.org/3",  
+  baseURL: "https://api.themoviedb.org/3",
   headers: {
-    Authorization: token},
+    Authorization: `Bearer ${apiKey}`
+  }
 });
